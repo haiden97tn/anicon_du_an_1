fb9763f8e217e827a7a9fd2dd33c818e3865b6be
haiden97tn <haiden97tn@gmail.com>
2021-04-19 10:07:58 +0700

    update file detail-project

--- frontend/src/app/components/detail-project/detail-project.component.ts
+++ frontend/src/app/components/detail-project/detail-project.component.ts
@@ -1,29 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Pipe } from '@angular/core';
+
 import { ActivatedRoute } from '@angular/router';
 import { CategoryService } from 'src/app/services/category.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-detail-project',
   templateUrl: './detail-project.component.html',
   styleUrls: ['./detail-project.component.css']
 })
+
+
 export class DetailProjectComponent implements OnInit {
 
   categoryId: string;
   category: any;
   YoutubeLink: string = '';
+  videoURL: string;
+  public safeUrl : SafeResourceUrl
 
-  constructor(private route: ActivatedRoute, private categoryService: CategoryService) { }
-
-  async ngOnInit() {
+  constructor(public _sanitizer: DomSanitizer, private route: ActivatedRoute, private categoryService: CategoryService,) {
     this.route.params.subscribe(params => {
       this.categoryId = params['xxx'];
     });
     this.categoryService.findById(this.categoryId).subscribe(data => {
       this.category = data.data.ListData[0];
-      this.YoutubeLink = (this.category.YoutubeLink).slice(32)
-      // console.log(this.YoutubeLink)
+      this.YoutubeLink = (this.category.YoutubeLink).slice(32);
+      this.safeUrl = this._sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${this.YoutubeLink}`);
+      console.log(this.safeUrl)
     })
+
+
   }
+
+  async ngOnInit() {
+
+
+
+  }
+  getLink(){
+    return `https://www.youtube.com/embed/${this.YoutubeLink}`
+
+  }
+
+
 
 }
